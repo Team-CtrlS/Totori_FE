32afc28da6465b3b3836f2caf1a9bb8ab853f67d
laura-jung <166522604+laura-jung@users.noreply.github.com>
2026-02-08 00:32:38 +0900

    Initial Commit

---
+++ Totori/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Totori
+//
+//  Created by 정윤아 on 2/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Totori/TotoriApp.swift
@@ -0,0 +1,17 @@
+//
+//  TotoriApp.swift
+//  Totori
+//
+//  Created by 정윤아 on 2/8/26.
+//
+
+import SwiftUI
+
+@main
+struct TotoriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
