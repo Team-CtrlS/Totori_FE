4f7ad99dc61edb98a489fcae0c113605d7a1176d
laura-jung <166522604+laura-jung@users.noreply.github.com>
2026-02-10 23:05:09 +0900

    [feat] 폰트 extension 추가

---
+++ Totori/Presentation/Common/Extensions/Font+.swift
@@ -0,0 +1,30 @@
+//
+//  Font+.swift
+//  Totori
+//
+//  Created by 정윤아 on 2/10/26.
+//
+
+import SwiftUI
+
+extension Font {
+    
+    // NotoSans_R
+    static let NotoSans_30_R: Font = .custom("NotoSansKR-Regular", size: 30)
+    static let NotoSans_24_R: Font = .custom("NotoSansKR-Regular", size: 24)
+    static let NotoSans_20_R: Font = .custom("NotoSansKR-Regular", size: 20)
+    static let NotoSans_18_R: Font = .custom("NotoSansKR-Regular", size: 18)
+    static let NotoSans_16_R: Font = .custom("NotoSansKR-Regular", size: 16)
+    static let NotoSans_14_R: Font = .custom("NotoSansKR-Regular", size: 14)
+    
+    // NotoSans_SB
+    static let NotoSans_30_SB: Font = .custom("NotoSansKR-SemiBold", size: 30)
+    static let NotoSans_24_SB: Font = .custom("NotoSansKR-SemiBold", size: 24)
+    static let NotoSans_20_SB: Font = .custom("NotoSansKR-SemiBold", size: 20)
+    static let NotoSans_18_SB: Font = .custom("NotoSansKR-SemiBold", size: 18)
+    static let NotoSans_16_SB: Font = .custom("NotoSansKR-SemiBold", size: 16)
+    static let NotoSans_14_SB: Font = .custom("NotoSansKR-SemiBold", size: 14)
+    
+    // NotoSans_B
+    static let NotoSans_30_B: Font = .custom("NotoSansKR-Bold", size: 30)
+}
