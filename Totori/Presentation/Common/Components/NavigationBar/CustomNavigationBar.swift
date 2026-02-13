349248fcda2ba74b7515f8dcb4f2a590835b626e
laura-jung <166522604+laura-jung@users.noreply.github.com>
2026-02-13 17:46:41 +0900

    [feat] 커스텀 네비게이션 바 구현

---
+++ Totori/Presentation/Common/Components/NavigationBar/CustomNavigationBar.swift
@@ -0,0 +1,89 @@
+//
+//  CustomNavigationBar.swift
+//  Totori
+//
+//  Created by 정윤아 on 2/13/26.
+//
+
+import SwiftUI
+
+enum NavigationBarType {
+    case text(String)
+    case textLogo
+    case logo
+}
+
+struct CustomNavigationBar<RightContent: View>: View {
+    
+    // MARK: - Properties
+    
+    let centerType: NavigationBarType
+    let leftAction: (() -> Void)?
+    let rightContent: RightContent
+    
+    // MARK: - Inits
+    
+    //화살표가 있는 경우
+    init (
+        centerType: NavigationBarType,
+        leftAction: (() -> Void)? = nil,
+        @ViewBuilder rightContent: () -> RightContent
+    ) {
+        self.centerType = centerType
+        self.leftAction = leftAction
+        self.rightContent = rightContent()
+    }
+    
+    //화살표가 없는 경우
+    init(
+        centerType: NavigationBarType,
+        leftAction: (() -> Void)? = nil,
+    ) where RightContent == EmptyView {
+        self.centerType = centerType
+        self.leftAction = leftAction
+        self.rightContent = EmptyView()
+    }
+    
+    var body: some View {
+        HStack {
+            if let action = leftAction {
+                Button(action: action) {
+                    Image(.leftPurple)
+                        .padding(20)
+                }
+            } else {
+                Spacer().frame(width: 44)
+            }
+            
+            Spacer()
+            
+            rightContent
+                .padding(.trailing, 20)
+        }
+        .frame(height: 50)
+        .overlay( centerView )
+        .background(Color.white)
+    }
+    
+    @ViewBuilder
+    private var centerView: some View {
+        switch centerType {
+        case .text(let title):
+            Text(title)
+                .font(.NotoSans_16_SB)
+                .foregroundColor(.tBlack)
+            
+        case .textLogo:
+            Image(.logoFlatPurple)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 11)
+            
+        case .logo:
+            Image(.icLogoPurple)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 30)
+        }
+    }
+}
