ab8821f495365f8fae08b9c79653b5a991da9516
laura-jung <166522604+laura-jung@users.noreply.github.com>
2026-03-07 04:43:31 +0900

    [chore] 경고 해결

--- Totori/Presentation/ReadStoryBook/View/BookInfoView.swift
+++ Totori/Presentation/ReadStoryBook/View/BookInfoView.swift
@@ -24,7 +24,8 @@ struct BookInfoView: View {
                 //TODO: - 이미지 파일로 교체하기
                 Rectangle()
                     .foregroundColor(.tGray)
-                    .frame(width: .infinity, height: 450)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 450)
                 
                 VStack (alignment: .leading, spacing: 10){
                     HStack {
